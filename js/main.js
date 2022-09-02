74f4ec524235ac11c9bd246aa4160422b987c997
aranramesh <svramesh692@gmail.com>
2022-09-02 17:05:23 +0530

    js objects

---
+++ js/main.js
@@ -0,0 +1,330 @@
+// const person = { 
+//     name: 'John',
+//     age: 20,
+//     native:"USA",
+//     dob:"29-02-2002"
+//  };
+//  console.log(person); 
+//  // 1
+ 
+//  const laptop = {
+//     brand :"lenovo",
+//     model :"e41",
+//     ram: "4gb"
+//  };
+//  console.log(laptop)
+//  // 2
+
+//  const car= {
+//     brand :"lamborgini",
+//     model :"urus",
+//     fueltype:"diesel",
+//     color:"orange"
+//  };
+//  console.log(car)
+//  // 3
+
+//  const mobile = {
+//     brand :"vivo",
+//     model :"u20",
+//     ram: "4gb",
+//     color:"blue"
+ 
+//  };
+//  console.log(mobile)
+ 
+//  // 4
+ 
+//  const comapny ={
+//     cname : "zoho",
+//     worker : "10000",
+//     location:"tenkasi",
+//     branch:"chennai"
+
+//  };
+//  console.log(company)
+
+//  // 5
+//  const student = {
+//      name : "ajith",
+//      age : 43,
+//      area:"kovai"
+
+
+//  };
+//  console.log(student)
+
+//  // 6
+ 
+//   const aadhar = {
+//      name:"kumar",
+//      age : 55,
+//      dob:30-01-1934
+
+//   };
+//   console.log("aadhar") 
+  
+//  //  7
+
+//  const watch={
+//      brand:"apple",
+//      color:"black",
+//      rupees:30000,
+//      seller :"apple",
+//      releaseyear:"2022"
+//  };
+//  console.log(watch)
+
+//  // 8
+
+
+//  const books ={
+//      author:"brain",
+//      bname:"timemanagement",
+//      rate:100,
+//      lang:"english",
+//      country:"india"
+//  };
+//  console.log(books)
+
+//  // 9
+
+//  const fabevy ={
+//      ceo:"velmurugan",
+//      location:"chennai",
+//      branch:"pavoorchatram",
+//      founddate:"2019",
+//      newbranch:"chennai"
+
+
+//  };
+//  console.log(fabevy)
+//  // 10
+
+//  const notebook ={
+//      brand:"classmate",
+//      mrp:"50",
+//      headoffice:"noida",
+//      fyear:"1998",
+//      awards:"superbrand",
+     
+
+
+//  };
+//  console.log(notebook)
+
+//  // 11
+
+//  const mail ={
+//      brand:"gmail",
+//      headcompany:"google",
+//      usercount:"1.5 Billion",
+//      service:"free",
+//      ceo:"sundhar"
+
+//  }; 
+//  console.log(mail)
+//  // 12
+
+
+// const predator={
+//     brand:"acer",
+//     type:"gaming laptop",
+//     ram:"16gb",
+//     graphics:"rtx3060",
+//     price:"60,000"
+// };
+// //    13
+
+// console.log(predator)
+
+// const headset={
+//     brand:"leaf",
+//     model:"ear",
+//     price:"999.Rs",
+//     head:"boat",
+//     year:"2021"
+// };
+
+// console.log(headset)
+
+// // 14
+
+// const channel={
+//     owner:"ramesh",
+//     cname:"aranvlogs",
+//     subs:"1.5million",
+//     year:"2023",
+//     monetization:"yes",
+//     income:"3000 dollar"
+// };
+
+// console.log(channel)
+
+// // 15
+
+// const hiring={
+//     experience:"1 to 2 years",
+//     salary:"4.5LPA",
+//     skills:"javascript,css3,HTML5,TypeScript",
+//     fresher:"allowed",
+//     company:"tcs"
+// };
+// console.log(hiring)
+
+// // 16
+
+// const monitor={
+//     brand:"benq",
+//     price:"11000",
+//     screensz:"45ch",
+//     type:"oled",
+//     model:"a54"
+// };
+// console.log(monitor)
+
+// // 17
+
+
+// const train={
+//     tname:"silambu express",
+//     timing:"3.30pm",
+//     spoint:"sencottah",
+//     epoint:"egmore",
+//     maxspeed:"120mph"
+// };
+// console.log(train)
+// // 18
+
+// const bus={
+//     bname:"vrl",
+//     owner:"aran buses",
+//     location:"chennai",
+//     headoffice:"tenkasi",
+//     totalbuses:"58"
+
+// };
+// console.log(bus)
+// // 19
+
+// const game={
+//     gname:"gta-v",
+//     minram:"8gb",
+//     graphicscard:"rtx1080",
+//     processor:"i5",
+//     size:"50gb"
+// };
+// console.log(game)
+// // 20
+
+// const country={
+//     cname:"india",
+//     population:"140crores",
+//     capital:"delhi",
+//     domesticproduct:"2.62 lakh crores()usd",
+//     money:"Rupees"
+    
+// };
+// console.log(country)
+// // 21
+
+
+// const AmericanFootball={
+//     Olympic: "No",
+//     WorldGames: "Yes", 
+//     country:"America",
+//     contact:"full",
+//     equipment:"football"
+// };
+// console.log(AmericanFootball)
+// // 22
+
+// const bike={
+//     model:"ninja h2",
+//     engine:"1000 CC",
+//     fueltype:"petrol",
+//     topspeed:"160",
+//     owner:"ramesh"
+// };
+// console.log(bike)
+
+// // 23
+
+// const honda={
+//     model:"civic",
+//     nickname:"beautyqueen",
+//     topspeed:"200-mph",
+//     color:"white",
+//     owner:honde
+// };
+// console.log(honda)
+
+// // 24
+
+
+// const chrome={
+//     work:"browser",
+//     owner:"google",
+//     speciality:"fast browser",
+//     size:"50mb",
+//     award:"most used browser"
+// };
+
+// console.log(chrome)
+// // 25
+
+// const vscode={
+//     use:"type-code",
+//     benifit:"separate code",
+//     owns:"microsoft",
+//     special:"most used coder",
+//     color:"blue"
+// };
+// console.log(vscode)
+// // 26
+
+// const windows={
+//     version="windows 10",
+//     keyprice:"199 usd",
+//     owner:"microsoft",
+//     relaeseyear:"2020",
+//     benifit:"most advanced version"
+// };
+// console.log(windows)
+
+// // 27
+
+// const playstation={
+//     varinat:"ps5",
+//     version:"latest version",
+//     price:"60000",
+//     owns:"sony corparation",
+//     country:"japan"
+// };
+// console.assertlog(playstation)
+
+// // 28
+
+// const pc={
+//     model:"hulk",
+//     processor:"i 9  10th generation",
+//     graphics:"rtx3090",
+//     price:"500000",
+//     ram:"32gb"
+// };
+// console.log(pc)
+
+// // 29
+
+// const facebook={
+//     owner:"mark zuckerberg",
+//     mothercompany:"meta",
+//     country:"America",
+//     earning:"2.5billion",
+//     speciality:"most used social media"
+    
+// };
+// console.log(facebook)
+
+// // 30
+
